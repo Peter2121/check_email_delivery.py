79c9875e84506f6275a804dd4554420713324135
Peter <peter@flytrace.com>
2018-11-30 17:38:57 +0100

    Add subject to check_imap_receive

--- check_imap_receive.py
+++ check_imap_receive.py
@@ -18,12 +18,14 @@ DEFAULT_PORT = 993
 DEFAULT_PROFILECONFIG = '/etc/nagios-plugins/check_email_delivery_credentials.ini'
 DEFAULT_WARN = 120
 DEFAULT_CRIT = 600
+DEFAULT_SUBJECT = 'TEST'
 
 parser = argparse.ArgumentParser(description='This program checks if the most recent mail in an IMAP inbox is not older than a certain time. Use this check as a companion to check_smtp_send.py, which sends test email messages.')
 parser.add_argument('-H', dest='host', metavar='host', required=True, help='IMAP host')
 parser.add_argument('--port', type=int, default=DEFAULT_PORT, help='IMAP port (default=%i)'%DEFAULT_PORT)
 parser.add_argument('--profile', required=True, help='credential profile in config file')
 parser.add_argument('--profileconfig', metavar='config.ini', default=DEFAULT_PROFILECONFIG, help='location of the config file (default=%s)'%DEFAULT_PROFILECONFIG)
+parser.add_argument('--subject', metavar='subject', default=DEFAULT_SUBJECT, help='Message subject')
 parser.add_argument('-w', type=int, metavar='warningseconds', dest='warn', default=DEFAULT_WARN, help='warn, if the most recent message is older than this value (default=%s)' % DEFAULT_WARN)
 parser.add_argument('-c', type=int, metavar='criticalseconds', dest='crit', default=DEFAULT_CRIT, help='critical, if the most recent message is older than this value (default=%s)' % DEFAULT_CRIT)
 
@@ -38,6 +40,9 @@ profile = args['profile']
 profileconfig = args['profileconfig']
 warn = args['warn']
 crit = args['crit']
+subject = args['subject']
+print('Searching for %s'% subject)
+srch = '(SUBJECT \"'+subject+'\")'
 
 config = ConfigParser.SafeConfigParser()
 config.read(profileconfig)
@@ -55,7 +60,8 @@ try:
     imap = imaplib.IMAP4_SSL(host, port)
     imap.login(username, password)
     imap.select()
-    typ, data = imap.search(None, 'ALL')
+#    typ, data = imap.search(None, 'ALL')
+    typ, data = imap.search(None, srch)
 
     pattern = re.compile(r'\(INTERNALDATE "(.+)"\)')
 
@@ -71,6 +77,10 @@ try:
 
     sortedmessages = sorted(messages, key=lambda x: x[0])
 
+    if len(sortedmessages) < 1:
+        print('No messages found')
+        sys.exit(EXIT_CRITICAL)
+
     mostrecent = sortedmessages[-1]
     for d,num in sortedmessages[:-1]:
         imap.store(num, '+FLAGS', '\\Deleted')
@@ -78,15 +88,15 @@ try:
     imap.expunge()
     imap.close()
     imap.logout()
-except IMAP4.abort as e:
+except imaplib.IMAP4.abort as e:
     # IMAP4 server errors cause this exception to be raised. This is a sub-class of IMAP4.error. Note that closing the instance and instantiating a new one will usually allow recovery from this exception.
     print e
     sys.exit(EXIT_CRITICAL)
-except IMAP4.readonly as e:
+except imaplib.IMAP4.readonly as e:
     # This exception is raised when a writable mailbox has its status changed by the server. This is a sub-class of IMAP4.error. Some other client now has write permission, and the mailbox will need to be re-opened to re-obtain write permission.
     print e
     sys.exit(EXIT_CRITICAL)
-except IMAP4.error as e:
+except imaplib.IMAP4.error as e:
     # Exception raised on any errors. The reason for the exception is passed to the constructor as a string.
     print e
     sys.exit(EXIT_CRITICAL)
